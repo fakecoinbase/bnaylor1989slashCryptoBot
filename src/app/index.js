03274b2c3c94cb63bdc40a4d319c212276ebc572
Ben Naylor <ben.naylor89@gmail.com>
2020-06-12 16:12:21 +0100

    Retrieve BTC Spot Price, connected to MongoDB instance

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,32 @@
-const pricing = require('pricing');
+const pricing = require('pricing')
+const database = require('database')
+const util = require('util')
 
-// import pricing from '../pricing/index'
+const setTimeoutPromise = util.promisify(setTimeout)
+pricing.setCurrency('BTC-GBP')
+
+const mainLoop = async () => {
+  const buyPrice = await pricing.getBuyPrice();
+  const sellPrice = await pricing.getSellPrice();
+  const spotPrice = await pricing.getSpotPrice();
+
+  console.log(`Buy: ${buyPrice.amount}`)
+  console.log(`Sell: ${sellPrice.amount}`)
+  console.log(`Spot: ${spotPrice.amount}`)
+
+  const time = 5 * 1000
+  await setTimeoutPromise(time)
+  mainLoop()
+}
+
 module.exports = {
-  start: async () => {
 
-    const buyPrice = await pricing.getBuyPrice();
-    const sellPrice = await pricing.getSellPrice();
+
+  start: async () => {
     
-    console.log(`Buy price: ${buyPrice.data.amount}`)
-    console.log(`Sell price: ${sellPrice.data.amount}`)
-  
+    await database.connect()
+    mainLoop()
+
   }
 }
 
--- src/pricing/index.js
+++ src/pricing/index.js
@@ -8,20 +8,30 @@ var client = new coinbase.Client({'apiKey': apiKey,
                          'apiSecret': apiSecret,
                           strictSSL: false});
 
+let currency = 'BTC-GBP'
+
 module.exports = {
 
-    currency: 'BTC-USD',
+    setCurrency: curr => {
+        currency; curr
+    },
+    getSpotPrice: async () => new Promise((resolve, reject) => {
+        const currencyPair = currency
+        client.getSpotPrice({ currencyPair }, (err, obj) => {
+            err ? reject(err) : resolve(obj.data)
+        })
+    }),
     getBuyPrice: async () => new Promise((resolve, reject) => {
-        const currencyPair = this.currency
+        const currencyPair = currency
         client.getBuyPrice({ currencyPair }, (err, obj) => {
-            err ? reject(err) : resolve(obj)
+            err ? reject(err) : resolve(obj.data)
         })
     }),
 
     getSellPrice: async () => new Promise((resolve, reject) => {
-        const currencyPair = this.currency
+        const currencyPair = currency
         client.getSellPrice({ currencyPair }, (err, obj) => {
-            err ? reject(err) : resolve(obj)
+            err ? reject(err) : resolve(obj.data)
         })
     })
 
